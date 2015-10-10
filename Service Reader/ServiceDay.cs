0c0c4336a782043bf62366a4fa7558afe26fa310
Richard Town <richard.town@gmail.com>
2015-10-10 12:24:17 +0100

    Created new classes ServiceDay and ServiceSheetSubmission

---
+++ Service Reader/ServiceDay.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+public class ServiceDay
+{
+    private DateTime dtServiceEntry = new DateTime();
+    private DateTime travelStartTime = new DateTime();
+    private DateTime arrivalTimeOnsite = new DateTime();
+    private DateTime departureTimeSite = new DateTime();
+    private DateTime travelEndTime = new DateTime();
+    private int mileage = 0;
+    private int dailyAllowance = 0;
+    private int overnightAllowance = 0;
+    private double totalTraveTime = 0;
+    private double totalTimeOnsite = 0;
+    private string dailyReport = "";
+    private string partsSuppliedToday = "";
+
+    public ServiceDay()
+    {
+
+    }
+}
---
+++ Service Reader/ServiceSheetSubmission.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+public class ServiceSheetSubmission
+{
+    private string submissionNumber = "";
+    private string customer = "";
+    private string addressLine1 = "";
+    private string addressLine2 = "";
+    private string townCity = "";
+    private string postcode = "";
+    private string customerContact = "";
+    private string customerPhoneNo = "";
+    private string machineMakeModel = "";
+    private string machineSerial = "";
+    private DateTime dtJobStart = new DateTime();
+    private string customerOrderNo = "";
+    private string mttJobNo = "";
+    private string jobDescription = "";
+    private double totalTimeOnsite = 0;
+    private double totalTravelTime = 0;
+    private double totalMileage = 0;
+    private int totalDailyAllowances = 0;
+    private int totalOvernightAllowances = 0;
+    private string jobStatus = "";
+    private string jobReport = "";
+    private string additionalFaultsFound = "";
+    private string partsRequired = "";
+    private string imageFollowup1Url = "";
+    private string imageFollowup2Url = "";
+    private string imageFollowup3Url = "";
+    private string imageFollowup4Url = "";
+    private string imageFollowup5Url = "";
+    private string customerSignatureUrl = "";
+    private string customerName = "";
+    private DateTime dtSigned = new DateTime();
+    private string mttEngSignatureUrl = "";
+    private serviceDay days();
+           
+}
