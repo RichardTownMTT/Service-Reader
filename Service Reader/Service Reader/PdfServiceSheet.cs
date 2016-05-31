f827b5027bac560f4793dca0dee1905488aa75fa
Richard Town <richard.town@gmail.com>
2016-05-31 21:48:32 +0100

    Added remaining tables to pdf. Started on footer

--- Service Reader/Service Reader/PdfServiceSheet.cs
+++ Service Reader/Service Reader/PdfServiceSheet.cs
@@ -13,12 +13,17 @@ namespace Service_Reader
         private ServiceSubmissionModel currentSheet;
         private string COLUMN_ONE_WIDTH = "6.43cm";
         private string COLUMN_TWO_WIDTH = "9.87cm";
-        private Color headerGrey = new Color(153, 153, 153);
-        private Color entryHeaderGrey = new Color(192, 192, 192);
-        private Color timesheetDayGrey = new Color(128, 128, 128);
+        private Color headerGrey = new Color(191, 191, 191);
+        private Color entryHeaderGrey = new Color(242, 242, 242);
+        private Color timesheetDayGrey = new Color(217, 217, 217);
+        private Color tableBorderColour = new Color(191, 191, 191);
+        private double borderWidth = 0.25;
 
         private Table jobDetailsTable;
         private Table timesheetTable;
+        private Table serviceReportTable;
+        private Table followupTable;
+        private Table signoffTable;
 
         public Boolean createPdfSheetForSubmission(ServiceSubmissionModel serviceSubmissionSheet)
         {
@@ -38,6 +43,10 @@ namespace Service_Reader
             createHeader();
             createJobDetailsSection();
             createTimesheetSection();
+            createServiceReportSection();
+            createFollowupSection();
+            createSignoffSection();
+            createFooter();
 
             PdfDocumentRenderer docRenderer = new PdfDocumentRenderer();
             docRenderer.Document = serviceSheetDoc;
@@ -47,6 +56,186 @@ namespace Service_Reader
 
             successful = true;
             return successful;
+        }
+
+        private void createFooter()
+        {
+            Section currentSection = (Section)serviceSheetDoc.Sections.LastObject;
+            Table footerTable = currentSection.Footers.Primary.AddTable();
+            footerTable.AddColumn("5cm");
+            footerTable.AddColumn("15cm");
+            footerTable.AddColumn("5cm");
+
+            Row footerRow1 = footerTable.AddRow();
+            footerTable.Style = "footerStyle";
+            footerRow1.Cells[0].AddParagraph("Logo");
+            Paragraph addressParagraph = footerRow1.Cells[1].AddParagraph();
+            addressParagraph.AddText("Machine Tool Technologies Ltd , 1H Ribble Court, 1 Meadway");
+
+            Row footerRow2 = footerTable.AddRow();
+            Paragraph addressPara2 = footerRow2.Cells[1].AddParagraph();
+            addressPara2.AddText("Shuttleworth Mead Business Park, Padiham, Lancashire, BB12 7NG, UK");
+
+            Row footerRow3 = footerTable.AddRow();
+            Paragraph addressPara3 = footerRow3.Cells[1].AddParagraph();
+            addressPara3.AddText("Tel. +44 (0) 845 077 9345  Fax. +44 (0) 1282 779 615  Web Address: www.mtt.uk.com");
+
+            //Merge the first cell, to contain the logo
+            footerRow1.Cells[0].MergeDown = 2;
+        }
+
+        private void createSignoffSection()
+        {
+            Section currentSection = (Section)serviceSheetDoc.Sections.LastObject;
+            signoffTable = currentSection.AddTable();
+
+            //Setup the table columns and borders
+            Column tableColumnn = signoffTable.AddColumn(COLUMN_ONE_WIDTH);
+            tableColumnn = signoffTable.AddColumn(COLUMN_TWO_WIDTH);
+            signoffTable.Borders.Color = tableBorderColour;
+            signoffTable.Borders.Width = borderWidth;
+
+            Row row1Title = signoffTable.AddRow();
+            row1Title.Cells[0].MergeRight = 1;
+            Paragraph signoffPara = row1Title.Cells[0].AddParagraph();
+            signoffPara.AddFormattedText("J", "allCapsFirstLetter");
+            signoffPara.AddFormattedText("OB ", "allCapsNextLetter");
+            signoffPara.AddFormattedText("S", "allCapsFirstLetter");
+            signoffPara.AddFormattedText("IGNOFF", "allCapsNextLetter");
+            row1Title.Cells[0].Shading.Color = headerGrey;
+
+            row1Title.KeepWith = 6;
+            Row row2Title = signoffTable.AddRow();
+            row2Title.Cells[0].MergeRight = 1;
+            Paragraph signoffParaCertify = row2Title.Cells[0].AddParagraph();
+            signoffParaCertify.AddText("I hereby certify that the service work has been carried out to my satisfaction");
+            row2Title.Cells[0].Shading.Color = entryHeaderGrey;
+
+            addLineToSignoffTable("Customer signature", currentSheet.CustomerSignatureUrl);
+            addLineToSignoffTable("Customer name", currentSheet.CustomerSignName);
+            addLineToSignoffTable("Date", currentSheet.DtSigned.ToShortDateString());
+            addLineToSignoffTable("MTT engineer signature", currentSheet.MttEngSignatureUrl);
+            addLineToSignoffTable("MTT engineer", currentSheet.EngineerFullName);
+        }
+
+        private void addLineToSignoffTable(string rowTitle, string rowData)
+        {
+            Paragraph currentParagraph;
+            Row currentRow = signoffTable.AddRow();
+            currentParagraph = currentRow.Cells[0].AddParagraph();
+            currentParagraph.AddText(rowTitle);
+            currentParagraph.Style = "sectionHeader";
+
+            currentParagraph = currentRow.Cells[1].AddParagraph();
+            currentParagraph.AddText(rowData);
+            currentParagraph.Style = "Normal";
+            currentRow.Cells[0].Shading.Color = entryHeaderGrey;
+        }
+
+        private void createFollowupSection()
+        {
+            Section currentSection = (Section)serviceSheetDoc.Sections.LastObject;
+            followupTable = currentSection.AddTable();
+
+            //Setup the table columns and borders
+            Column tableColumnn = followupTable.AddColumn(COLUMN_ONE_WIDTH);
+            tableColumnn = followupTable.AddColumn(COLUMN_TWO_WIDTH);
+            followupTable.Borders.Color = tableBorderColour;
+            followupTable.Borders.Width = borderWidth;
+
+            Row row1Title = followupTable.AddRow();
+            row1Title.Cells[0].MergeRight = 1;
+            Paragraph followupPara = row1Title.Cells[0].AddParagraph();
+            followupPara.AddFormattedText("F", "allCapsFirstLetter");
+            followupPara.AddFormattedText("OLLOW-UP ", "allCapsNextLetter");
+            followupPara.AddFormattedText("W", "allCapsFirstLetter");
+            followupPara.AddFormattedText("ORK", "allCapsNextLetter");
+            row1Title.Cells[0].Shading.Color = headerGrey;
+
+            row1Title.KeepWith = 9;
+
+            addLineToFollowupTable("Additional faults found", currentSheet.AdditionalFaultsFound);
+            addLineToFollowupTable("Parts required", currentSheet.PartsForFollowup);
+            addLineToFollowupTable("Quote required", currentSheet.QuoteRequired.ToString());
+
+            Row rowImages= followupTable.AddRow();
+            rowImages.Cells[0].MergeRight = 1;
+            Paragraph followupImagesPara = rowImages.Cells[0].AddParagraph();
+            followupImagesPara.AddFormattedText("F", "allCapsFirstLetter");
+            followupImagesPara.AddFormattedText("OLLOW-UP WORK IMAGES", "allCapsNextLetter");
+            rowImages.Cells[0].Shading.Color = headerGrey;
+
+            addLineToFollowupTable("Image 1", currentSheet.Image1Url);
+            addLineToFollowupTable("Image 2", currentSheet.Image2Url);
+            addLineToFollowupTable("Image 3", currentSheet.Image3Url);
+            addLineToFollowupTable("Image 4", currentSheet.Image4Url);
+            addLineToFollowupTable("Image 5", currentSheet.Image5Url);
+
+            //Add a space before the next table
+            currentSection.AddParagraph();
+        }
+
+        private void addLineToFollowupTable(string rowTitle, string rowData)
+        {
+            Paragraph currentParagraph;
+            Row currentRow = followupTable.AddRow();
+            currentParagraph = currentRow.Cells[0].AddParagraph();
+            currentParagraph.AddText(rowTitle);
+            currentParagraph.Style = "sectionHeader";
+
+            currentParagraph = currentRow.Cells[1].AddParagraph();
+            currentParagraph.AddText(rowData);
+            currentParagraph.Style = "Normal";
+            currentRow.Cells[0].Shading.Color = entryHeaderGrey;
+        }
+
+        private void createServiceReportSection()
+        { 
+            Section currentSection = (Section)serviceSheetDoc.Sections.LastObject;
+            serviceReportTable = currentSection.AddTable();
+
+            //Setup the table columns and borders
+            Column tableColumnn = serviceReportTable.AddColumn(COLUMN_ONE_WIDTH);
+            tableColumnn = serviceReportTable.AddColumn(COLUMN_TWO_WIDTH);
+            serviceReportTable.Borders.Color = tableBorderColour;
+            serviceReportTable.Borders.Width = borderWidth;
+
+            Row row1Title = serviceReportTable.AddRow();
+            row1Title.Cells[0].MergeRight = 1;
+            Paragraph serviceReportPara = row1Title.Cells[0].AddParagraph();
+            serviceReportPara.AddFormattedText("S", "allCapsFirstLetter");
+            serviceReportPara.AddFormattedText("ERVICE ", "allCapsNextLetter");
+            serviceReportPara.AddFormattedText("R", "allCapsFirstLetter");
+            serviceReportPara.AddFormattedText("EPORT", "allCapsNextLetter");
+            row1Title.Cells[0].Shading.Color = headerGrey;
+
+            row1Title.KeepWith = 8;
+
+            addLineToServiceReportTable("Total travel time", currentSheet.TotalTravelTime.ToString());
+            addLineToServiceReportTable("Total time onsite", currentSheet.TotalTimeOnsite.ToString());
+            addLineToServiceReportTable("Total mileage", currentSheet.TotalMileage.ToString());
+            addLineToServiceReportTable("Total daily allowances", currentSheet.TotalDailyAllowances.ToString());
+            addLineToServiceReportTable("Total overnight allowances", currentSheet.TotalOvernightAllowances.ToString());
+            addLineToServiceReportTable("Total barrier payments", currentSheet.TotalBarrierPayments.ToString());
+            addLineToServiceReportTable("Job status", currentSheet.JobStatus);
+            addLineToServiceReportTable("Job report", currentSheet.FinalJobReport);
+
+            //Add a space before the next table
+            currentSection.AddParagraph();
+        }
+
+        private void addLineToServiceReportTable(string rowTitle, string rowData)
+        {
+            Paragraph currentParagraph;
+            Row currentRow = serviceReportTable.AddRow();
+            currentParagraph = currentRow.Cells[0].AddParagraph();
+            currentParagraph.AddText(rowTitle);
+            currentParagraph.Style = "sectionHeader";
+
+            currentParagraph = currentRow.Cells[1].AddParagraph();
+            currentParagraph.AddText(rowData);
+            currentParagraph.Style = "Normal";
+            currentRow.Cells[0].Shading.Color = entryHeaderGrey;
         }
 
         private void createTimesheetSection()
@@ -59,8 +248,8 @@ namespace Service_Reader
             //Setup the table columns and borders
             Column tableColumnn = timesheetTable.AddColumn(COLUMN_ONE_WIDTH);
             tableColumnn = timesheetTable.AddColumn(COLUMN_TWO_WIDTH);
-            timesheetTable.Borders.Color = Colors.Black;
-            timesheetTable.Borders.Width = 0.25;
+            timesheetTable.Borders.Color = tableBorderColour;
+            timesheetTable.Borders.Width = borderWidth;
 
             int noOfDays = currentSheet.ServiceTimesheets.Count;
             int counter = 0;
@@ -81,8 +270,10 @@ namespace Service_Reader
                 addDayToTimesheet(currentDay, counter);
 
                 counter = counter + 1;
-            } 
+            }
 
+            //Add a gap before the next section
+            currentSection.AddParagraph();
         }
 
         private void addDayToTimesheet(ServiceDayModel currentDay, int dayCounter)
@@ -159,7 +350,11 @@ namespace Service_Reader
             MigraDoc.DocumentObjectModel.Style allCapsNextLetterStyle = serviceSheetDoc.Styles.AddStyle("allCapsNextLetter", "Normal");
             allCapsNextLetterStyle.Font.Size = 9;
             allCapsNextLetterStyle.Font.Bold = true;
-            
+
+            MigraDoc.DocumentObjectModel.Style footerStyle = serviceSheetDoc.Styles.AddStyle("footerStyle", "Normal");
+            footerStyle.Font.Size = 6;
+            footerStyle.Font.Name = "Arial";
+            footerStyle.Font.Color = headerGrey;
         }
 
         private void createJobDetailsSection()
@@ -170,8 +365,8 @@ namespace Service_Reader
             //Setup the table columns and borders
             Column tableColumnn = jobDetailsTable.AddColumn(COLUMN_ONE_WIDTH);
             tableColumnn = jobDetailsTable.AddColumn(COLUMN_TWO_WIDTH);
-            jobDetailsTable.Borders.Color = Colors.Black;
-            jobDetailsTable.Borders.Width = 0.25;
+            jobDetailsTable.Borders.Color = tableBorderColour;
+            jobDetailsTable.Borders.Width = borderWidth;
 
             Row row1Title = jobDetailsTable.AddRow();
             row1Title.Cells[0].MergeRight = 1;
